a2c154815863117a4cf71b813eb55ccff1a5c2f4
rhc <chenhairong@itouchtv.cn>
2019-05-31 18:36:38 +0800

    mac socket

---
+++ EazySocket/Mac/EasyTcpServer/EasyTcpServer/depends/include/Cell.hpp
@@ -0,0 +1,39 @@
+#ifndef _CELL_HPP_
+#define _CELL_HPP_
+
+// SOCKET
+#ifdef _WIN32
+	#define FD_SETSIZE      10240  //10k
+	#define WIN32_LEAN_AND_MEAN
+	#define _WINSOCK_DEPRECATED_NO_WARNINGS
+	#include<windows.h>
+	#include<WinSock2.h>
+	#pragma comment(lib,"ws2_32.lib")
+#else
+	#include<unistd.h> //uni std
+	#include<arpa/inet.h>
+	#include<string.h>
+	#include<signal.h>
+
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
+
+// Customer Class
+#include"MessageHeader.hpp"
+//#include"CellObjectPool.hpp"
+#include"CellTimestamp.hpp"
+#include"CellTask.hpp"
+#include"CellLog.hpp"
+//
+#include<stdio.h>
+
+//缓冲区最小单元大小
+#ifndef RECV_BUFF_SZIE
+    #define RECV_BUFF_SIZE 8192
+    #define SEND_BUFF_SIZE 10240
+#endif
+
+
+#endif
---
+++ EazySocket/Mac/EasyTcpServer/EasyTcpServer/depends/include/CellMsgStream.hpp
@@ -0,0 +1,83 @@
+#ifndef _CELL_MSG_STREAM_HPP_
+#define _CELL_MSG_STREAM_HPP_
+
+#include"MessageHeader.hpp"
+#include"CellStream.hpp"
+
+//消息数据字节流
+class CellReadStream :public CellStream
+{
+public:
+	CellReadStream(netmsg_DataHeader* header)
+		:CellStream((char*)header, header->dataLength)
+	{
+
+	}
+
+	CellReadStream(char* pData, int nSize, bool bDelete = false)
+		:CellStream(pData, nSize,bDelete)
+	{
+		push(nSize);
+		//预先读取消息长度
+		//ReadInt16();
+		//预先读取消息命令
+		//getNetCmd();
+	}
+
+	uint16_t getNetCmd()
+	{
+		uint16_t cmd = CMD_ERROR;
+		Read<uint16_t>(cmd);
+		return cmd;
+	}
+};
+
+//消息数据字节流
+class CellWriteStream :public CellStream
+{
+public:
+	CellWriteStream(char* pData, int nSize, bool bDelete = false)
+		:CellStream(pData, nSize, bDelete)
+	{
+		//预先占领消息长度所需空间
+		Write<uint16_t>(0);
+	}
+
+	CellWriteStream(int nSize = 1024)
+		:CellStream(nSize)
+	{
+		//预先占领消息长度所需空间
+		Write<uint16_t>(0);
+	}
+    
+    void setNetCmd(uint16_t cmd)
+    {
+        Write<uint16_t>(cmd);
+    }
+    
+    bool WriteString(const char* str, int len)
+    {
+        return WriteArray(str, len);
+    }
+    
+    bool WriteString(const char* str)
+    {
+        return WriteArray(str, strlen(str));
+    }
+    
+    bool WriteString(std::string& str)
+    {
+        return WriteArray(str.c_str(), str.length());
+    }
+    
+    void finsh()
+    {
+        int pos = length();
+        setWritePos(0);
+        Write<uint16_t>(pos);
+        setWritePos(pos);
+    }
+};
+
+
+#endif // !_CELL_MSG_STREAM_HPP_
