339a5a9feab7e3916e6811b0dc59df37aa38dfdc
rhc <chenhairong@itouchtv.cn>
2019-05-31 18:36:38 +0800

    mac socket

---
+++ EazySocket/Mac/EasyTcpClient/EasyTcpClient/depends/include/CellSemaphore.hpp
@@ -0,0 +1,45 @@
+﻿#ifndef _CELL_SEMAPHOER_HPP_
+#define _CELL_SEMAPHOER_HPP_
+
+#include<chrono>
+#include<thread>
+#include<condition_variable>
+
+class CellSemaphore
+{
+public:
+	// thread block
+	void wait()
+	{
+		std::unique_lock<std::mutex>lock(_mutex);
+		if (--_wait < 0)
+		{
+			_cv.wait(lock, [this] ()->bool {
+				return _wakeup > 0;
+			});
+			--_wakeup;
+		}
+	}
+
+	// thread wakeup
+	void wakeup()
+	{
+		std::lock_guard<std::mutex> lock(_mutex);
+		if (++_wait <= 0) {
+			++_wakeup;
+			_cv.notify_one();
+		}
+	}
+
+private:
+	std::mutex _mutex;
+	// block variable
+	std::condition_variable _cv;
+	// wait for count 
+	int _wait = 0;
+	// wakeup for count
+	int _wakeup = 0;
+};
+
+
+#endif
---
+++ EazySocket/Mac/EasyTcpClient/EasyTcpClient/depends/include/CellTimestamp.hpp
@@ -0,0 +1,71 @@
+﻿#ifndef _CELL_TIMESTAMP_HPP_
+#define _CELL_TIMESTAMP_HPP_
+//#include <windows.h>
+#include<chrono>
+using namespace std::chrono;
+
+class CellTime
+{
+public:
+	// get the current  time. millisec
+	static time_t  getNowInMilliSec()
+	{
+		return duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
+	}
+};
+
+class CellTimestamp
+{
+public:
+	CellTimestamp()
+    {
+        //QueryPerformanceFrequency(&_frequency);
+        //QueryPerformanceCounter(&_startCount);
+		update();
+    }
+    ~CellTimestamp()
+    {}
+
+    void update()
+    {
+        //QueryPerformanceCounter(&_startCount);
+		_begin = high_resolution_clock::now();
+    }
+    /**
+    *   获取当前秒
+    */
+    double getElapsedSecond()
+    {
+        return  getElapsedTimeInMicroSec() * 0.000001;
+    }
+    /**
+    *   获取毫秒
+    */
+    double getElapsedTimeInMilliSec()
+    {
+        return this->getElapsedTimeInMicroSec() * 0.001;
+    }
+    /**
+    *   获取微妙
+    */
+    long long getElapsedTimeInMicroSec()
+    {
+		/*
+        LARGE_INTEGER endCount;
+        QueryPerformanceCounter(&endCount);
+
+        double  startTimeInMicroSec =   _startCount.QuadPart * (1000000.0 / _frequency.QuadPart);
+        double  endTimeInMicroSec   =   endCount.QuadPart * (1000000.0 / _frequency.QuadPart);
+
+        return  endTimeInMicroSec - startTimeInMicroSec;
+		*/
+		
+		return duration_cast<microseconds>(high_resolution_clock::now() - _begin).count();
+    }
+protected:
+    //LARGE_INTEGER   _frequency;
+    //LARGE_INTEGER   _startCount;
+	time_point<high_resolution_clock> _begin;
+};
+
+#endif 
